b54520f94250560f6190096ac417dfd867916de6
NannyWa96 <127409328+HindElMouta@users.noreply.github.com>
2025-03-04 16:43:09 +0100

    renamed:    readme.txt -> README.md 	modified:   get_next_line.h 	deleted:    test/test.txt 	deleted:    test/test2.txt 	deleted:    test/test3.txt

--- get_next_line.h
+++ get_next_line.h
@@ -25,12 +25,13 @@
 /*--fonctions--*/
 
 char	*get_next_line(int fd);
+char	*ft_join_and_free(char *var_static, char *var_temp);
+char	*ft_next(char *var_static);
+char	*ft_line(char *var_static);
+char	*read_file(int fd, char *var_static, ssize_t	byte_read);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
-
-void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
-
 size_t	ft_strlen(const char *str);
 
 #endif
